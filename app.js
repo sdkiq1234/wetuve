64c00f9294abc1482cd23f73cc9f8fa35b7dc26e
sdkiq1234 <sdkiq123@naver.com>
2021-03-26 17:39:56 +0900

    controller, pug

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
-const handleHome = (req, res) => res.send("hello from home");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
-app.use(helmet());
-app.use(morgan("dev"));
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
-app.get("/profile", handleProfile);
-app.get("/", handleHome);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
