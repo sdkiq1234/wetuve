8160c9ccafc44d183ed654d59b3ea312c22a5252
sdkiq1234 <sdkiq123@naver.com>
2021-04-05 08:29:38 +0900

    mongodb

--- db.js
+++ db.js
@@ -1,54 +1,17 @@
-export const videos = [
-  {
-    id: 444444,
-    title: "Video 444",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 444444,
-      name: "Daekyu",
-      email: "sdkiq123@naver.com",
-    },
-  },
-  {
-    id: 333333,
-    title: "Video 333",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 333333,
-      name: "Daekyu",
-      email: "sdkiq123@naver.com",
-    },
-  },
-  {
-    id: 222222,
-    title: "Video 222",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 222222,
-      name: "Daekyu",
-      email: "sdkiq123@naver.com",
-    },
-  },
-  {
-    id: 111111,
-    title: "Video 1111",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 111111,
-      name: "Daekyu",
-      email: "sdkiq123@naver.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = (error) =>
+  console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
