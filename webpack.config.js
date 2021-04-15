20bf9b697624a2652df3c3feb4e9ef017c9af10e
sdkiq1234 <sdkiq123@naver.com>
2021-04-15 17:33:24 +0900

    webpack setting-1

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+  entry: ENTRY_FILE,
+  mode: MODE,
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: MiniCssExtractPlugin.extract({
+          loader: "css-loader",
+        }),
+      },
+    ],
+  },
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].[format]",
+  },
+};
+
+module.exports = config;
