787f69eef6a4cc6b330f871ee91583cd6c4f2677
sdkiq1234 <sdkiq123@naver.com>
2021-05-14 17:20:13 +0900

    nenewer-1

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-import "./db";
+import "../db";
 import dotenv from "dotenv";
-import app from "./app";
-import "./models/Video";
-import "./models/Comment";
+import app from "../app";
+import "../models/Video";
+import "../models/Comment";
 
 dotenv.config();
 
@@ -11,4 +11,5 @@ const PORT = process.env.PORT || 4000;
 const handleListening = () =>
   console.log(`✅ Listening on: http://localhost:${PORT}`);
 
+// port해당 포트에서 request하기를 기다림
 app.listen(PORT, handleListening);
